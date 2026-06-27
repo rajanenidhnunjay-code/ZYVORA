5fb240a716345599c3284ea04b2c12be4338df26
Dhanunjay <rajanenidhnunjay@gmail.com>
2026-06-28 01:26:33 +0530

    chore: silence typescript css module warning and update main layout

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,8 @@
-import {StrictMode} from 'react';
-import {createRoot} from 'react-dom/client';
+import { Analytics } from '@vercel/analytics/react';
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
 import App from './App.tsx';
+// @ts-ignore
 import './index.css';
 
 createRoot(document.getElementById('root')!).render(
